051ddfed2e80b0436f338518fb3f60a979df0615
Auca17 <augurufino@gmail.com>
2026-04-01 11:52:01 -0300

    docker_intro01

---
+++ docker_intro01/con_dependencias.py
@@ -0,0 +1,7 @@
+"""Script que usa una biblioteca externa."""
+
+import requests
+
+response = requests.get("https://httpbin.org/get")
+print(f"Status: {response.status_code}")
+print(f"Datos: {response.json()['origin']}")
